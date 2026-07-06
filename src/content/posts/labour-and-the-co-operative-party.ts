7922d22d169175694a33851f4b660f7b37dc5083
Derek Yuan <87529592+derekyuan1000@users.noreply.github.com>
2026-07-06 21:11:57 +0100

    story: The story of the Co-operative party, and how the 4th biggest British party has gone unnoticed

---
+++ src/content/posts/labour-and-the-co-operative-party.ts
@@ -0,0 +1,12 @@
+import type { Post } from "@/content/types";
+
+export const post: Post = {
+  title: "Britain’s Fourth Biggest Party that also sells you eggs",
+  subtitle:
+    "The story of the Co-operative party, and how the 4th biggest British party has gone unnoticed",
+  url: "https://medium.com/@derekyuan10000/britains-fourth-biggest-party-that-also-sells-you-eggs-0b1681311cfa?source=friends_link&sk=6e8a9d6f9c15608e66083cc52dd0c17f",
+  image: "https://miro.medium.com/v2/resize:fit:640/format:webp/0*IN3oBLfx1FOSjA7X.jpg",
+  tags: ["Politics"
+  ],
+  date: "2026-06-20",
+};
