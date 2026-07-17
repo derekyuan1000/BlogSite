3da2890a03ac69f405b41b569d3990a24eff6803
Derek Yuan <87529592+derekyuan1000@users.noreply.github.com>
2026-07-17 17:28:45 +0800

    Update post details for pairs trading strategy

---
+++ src/content/posts/pairs-trading-stratagy.ts
@@ -0,0 +1,12 @@
+import type { Post } from "@/content/types";
+
+export const post: Post = {
+  title:
+    "Pairs Trading: The Wall Street Strategy That Prints Money Until it Didn’t",
+  subtitle:
+    "Pairs Trading: The Wall Street Strategy That Prints Money Until it Didn’t A physicist walks into a trading floor. No, that’s not the start of a bad joke, that’s really how it started.",
+  url: "https://medium.com/@derekyuan10000/pairs-trading-the-wall-street-strategy-that-prints-money-until-it-didnt-765e3206f91c?source=friends_link&sk=bbaa532de9fbc7b4f2bfa185dc559c29",
+  image: "https://miro.medium.com/v2/resize:fit:786/format:webp/0*kEJeBFdQsZW9uH2w",
+  tags: ["Finance"],
+  date: "2026-07-10",
+};
