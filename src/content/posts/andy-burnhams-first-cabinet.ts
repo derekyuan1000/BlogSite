3a5d145304c7c2394c94c434462ec046f5e43ea9
Derek Yuan <87529592+derekyuan1000@users.noreply.github.com>
2026-08-02 00:08:03 +0800

    Create andy-burnhams-first-cabinet.ts

---
+++ src/content/posts/andy-burnhams-first-cabinet.ts
@@ -0,0 +1,12 @@
+import type { Post } from "@/content/types";
+
+export const post: Post = {
+  title:
+    "A New Start for Burnham?: What Burnham’s First Cabinet Actually Tells Us",
+  subtitle:
+    "A surprise pick in Healey, A purge of Starmer Loyalists. How much political capital does Burnham have?",
+  url: "https://medium.com/@derekyuan10000/a-new-start-for-burnham-what-burnhams-first-cabinet-actually-tells-us-1248ec977520?source=friends_link&sk=e51258da3319a0ea7395febe4c129f0d",
+  image: "https://miro.medium.com/v2/resize:fit:720/format:webp/0*yRmu68MGxAgtkw_C.jpg",
+  tags: ["Politics"],
+  date: "2026-07-25",
+};
