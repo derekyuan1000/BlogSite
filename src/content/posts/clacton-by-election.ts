35b85b6f66d76028d92d3f177ef970e262fead05
Derek Yuan <87529592+derekyuan1000@users.noreply.github.com>
2026-08-02 00:11:11 +0800

    Create clacton-by-election.ts

---
+++ src/content/posts/clacton-by-election.ts
@@ -0,0 +1,12 @@
+import type { Post } from "@/content/types";
+
+export const post: Post = {
+  title:
+    "Anthropic vs the USA: The Release of Fable and Mythos 5, US Government shuts down the strongest AI model ever?",
+  subtitle:
+    "Reform's poll lead has vanished, the scandal won't die, and a man in a bin might finish second",
+  url: "https://medium.com/@derekyuan10000/could-a-bin-beat-reform-the-clacton-by-election-explained-9d4f75365c9e?source=friends_link&sk=6b7ca54dca36dc0cdf14eb513f11a70a",
+  image: "https://miro.medium.com/v2/format:webp/0*mwmoA156gCLsil8_",
+  tags: ["Politics"],
+  date: "2026-08-01",
+};
